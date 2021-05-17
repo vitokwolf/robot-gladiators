c64091e04a594d2b3d75aaa473512f78244bc330
Victor Lupascu <sinajeen@gmail.com>
2021-05-16 20:38:54 -0500

    created first function and var

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,19 @@
-window.alert("This is an alert! JavaScript is running!");
+// window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+  }
+
+var playerName = window.prompt("what is your robot's name?");
+// Note the lack of quotation marks around playerName
+// window.alert(playerName);
+// What is this?
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+// fight();
