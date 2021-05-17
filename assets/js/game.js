b75d1623da16310ebe70291499035ed899aa27bd
Victor Lupascu <sinajeen@gmail.com>
2021-05-16 21:01:16 -0500

    added first round

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,19 +1,31 @@
-// window.alert("This is an alert! JavaScript is running!");
-// this creates a function named "fight"
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-function fight() {
-    window.alert("The fight has begun!");
-  }
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
 
-var playerName = window.prompt("what is your robot's name?");
-// Note the lack of quotation marks around playerName
-// window.alert(playerName);
-// What is this?
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message");
-// this will do math and log 20
-console.log(10 + 10);
-// what is this?
-console.log("Our robot's name is " + playerName);
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-// fight();
+var fight = function() {
+    // Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+  
+    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+  enemyHealth= enemyHealth - playerAttack;
+    // Log a resulting message to the console so we know that it worked.
+    console.log(
+  playerName + " attacked " + enemyName + ". " + enemyName + " now has "+ enemyHealth + " health remaining."
+    );
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+  playerHealth=playerHealth-enemyAttack;
+
+    // Log a resulting message to the console so we know that it worked.
+    console.log(
+        enemyName+" attacked "+playerName+". "+playerName+" now has "+playerHealth+" health remaining."
+    );
+  };
+
+fight();
